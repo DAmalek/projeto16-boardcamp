16dbb9f89bfd6b9186ff0bdf02d800f73c34ef16
Daniel Abdel <danielmalek101@gmail.com>
2023-02-13 12:04:08 -0300

    feat customer route

--- src/controllers/custumer.Controller.js
+++ src/controllers/custumer.Controller.js
@@ -24,3 +24,41 @@ export async function listCustomers(req, res) {
     res.status(500).send(error.message);
   }
 }
+
+export async function listOneCustomer(req, res) {
+  const { id } = req.params;
+
+  try {
+    const customer = await db.query('SELECT * FROM customers where "id" = $1', [
+      id,
+    ]);
+
+    if (customer.rows.length === 0) return res.sendStatus(404);
+    return res.send(customer.rows[0]);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+}
+
+export async function updateCustomer(req, res) {
+  const { id } = req.params;
+
+  const { name, phone, cpf, birthday } = req.body;
+  try {
+    const cpfUsed = await db.query(
+      'SELECT id FROM customers WHERE "cpf" = $1 AND "id" != $2',
+      [cpf, id]
+    );
+
+    if (cpfUsed.rows.length > 0) return res.sendStatus(409);
+
+    await db.query(
+      'UPDATE customers SET "name" = $1, "phone" = $2, "cpf" = $3, "birthday" = $4 WHERE "id" = $5',
+      [name, phone, cpf, birthday, id]
+    );
+
+    return res.sendStatus(200);
+  } catch (err) {
+    return res.status(500).send(err);
+  }
+}
--- src/routes/custumers.routes.js
+++ src/routes/custumers.routes.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 import {
   insertCustomer,
   listCustomers,
+  listOneCustomer,
+  updateCustomer,
 } from "../controllers/custumer.Controller.js";
 import { custumersValidation } from "../middlewares/custumers.middleware.js";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
@@ -10,12 +12,18 @@ import { schemaCustumers } from "../schemas/schema.custumers.js";
 const customersRoute = Router();
 
 customersRoute.get("/customers", listCustomers);
+customersRoute.get("/customers/:id", listOneCustomer);
 customersRoute.post(
   "/customers",
   validateSchema(schemaCustumers),
   custumersValidation,
   insertCustomer
 );
-customersRoute.put("/customers");
+customersRoute.put(
+  "/customers/:id",
+  validateSchema(schemaCustumers),
+
+  updateCustomer
+);
 
 export default customersRoute;
